0e838386ef6aadb3a3bd56906d2faee5ac2e615d
Samar-exe <samarik2004@gmail.com>
2024-06-06 21:56:05 +0530

    Refactor variables and add more functionality
    Renamed some variables and attached an onclick event listener to the
    equals to button to execute the operate function. the parameters passed
    in the operate function is just for testing

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ function div(a, b) {
   return a / b;
 }
 function operate(operator, num1, num2) {
-
+  switch (operator) {
+    case "+":
+      console.log(add(num1, num2));
+      break;
+    case "-":
+      console.log(sub(num1, num2));
+      break;
+    case "*":
+      console.log(mult(num1, num2));
+      break;
+    case "/":
+      console.log(div(num1, num2));
+      break;
+    default:
+      console.log("Please select a valid operator");
+      break;
+  }
 }
 
 //Necessary Variable Declaration.
-let firstNum = [];
+let num1Array = [];
 let operator = [];
-let secondNum = [];
+let num2Array = [];
 const numbers = [];
 let display = document.querySelector("#display-container")
 const clear = document.querySelector("#clear");
@@ -26,13 +42,19 @@ for (let i = 0; i <= 9; i++) {
   numbers[i] = document.querySelector("#num" + i);
 };
 const operators = [];
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= 5; i++) {
   operators[i] = document.querySelector("#btn" + i);
 };
 
 // to store the numbers in the firstNum array
 numbers.map((item) => {
-  return item.addEventListener("click", () => { return firstNum.push(item.textContent) });
+  return item.addEventListener("click", () => { return num1Array.push(item.textContent) });
 })
 
 clear.addEventListener("click", () => { display.textContent = " " })
+
+//to run the opearate function when equals to sign is pressed.
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+  console.log(operate("**", parseInt(num1Array.join("")), 2));
+})
