47cb3fd79dbdc5da752e8e72914a09b257181767
Samar-exe <samarik2004@gmail.com>
2024-06-13 02:08:00 +0530

    Store the essential values in variables

---
+++ script.js
@@ -0,0 +1,9 @@
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+let decimal = document.querySelector(".decimal");
+
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+
+let prevScreen = document.querySelector(".previous");
+let currScreen = document.querySelector(".current")
